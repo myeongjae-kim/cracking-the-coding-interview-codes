09c3e3ab74c200228db4764a3dd7d4cf916fd0db
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2018-05-31 22:12:29 -0500

    the_grid_search

---
+++ hackerrank/the_grid_search.cc
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the gridSearch function below.
+string gridSearch(vector<string> G, vector<string> P) {
+  int row_size = P.size();
+  int col_size = P[0].size();
+  
+  for(int row = 0; row + P.size() <= G.size(); row++) {
+    for(int col = 0; col + P[0].size() <= G[0].size(); col++) {
+      //cout << row << ' ' << col << endl;
+      
+      for(int r = 0; r < P.size(); r++) {
+        for(int c = 0; c < P[0].size(); c++) {
+          if(G[row + r][col + c] != P[r][c]) {
+            goto not_match;
+          }
+        }
+      }
+      
+      return "YES";
+      not_match:;
+    }
+  }
+  
+  //cout << endl;
+
+  return "NO";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string RC_temp;
+        getline(cin, RC_temp);
+
+        vector<string> RC = split_string(RC_temp);
+
+        int R = stoi(RC[0]);
+
+        int C = stoi(RC[1]);
+
+        vector<string> G(R);
+
+        for (int i = 0; i < R; i++) {
+            string G_item;
+            getline(cin, G_item);
+
+            G[i] = G_item;
+        }
+
+        string rc_temp;
+        getline(cin, rc_temp);
+
+        vector<string> rc = split_string(rc_temp);
+
+        int r = stoi(rc[0]);
+
+        int c = stoi(rc[1]);
+
+        vector<string> P(r);
+
+        for (int i = 0; i < r; i++) {
+            string P_item;
+            getline(cin, P_item);
+
+            P[i] = P_item;
+        }
+
+        string result = gridSearch(G, P);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+vector<string> split_string(string input_string) {
+    string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+        return x == y and x == ' ';
+    });
+
+    input_string.erase(new_end, input_string.end());
+
+    while (input_string[input_string.length() - 1] == ' ') {
+        input_string.pop_back();
+    }
+
+    vector<string> splits;
+    char delimiter = ' ';
+
+    size_t i = 0;
+    size_t pos = input_string.find(delimiter);
+
+    while (pos != string::npos) {
+        splits.push_back(input_string.substr(i, pos - i));
+
+        i = pos + 1;
+        pos = input_string.find(delimiter, i);
+    }
+
+    splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+
+    return splits;
+}
